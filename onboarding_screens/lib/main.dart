b1e0be7c78cd32c1bc8f6d2755e74cf1a01ce24c
Bit Bytes <apatole479@gmail.com>
2020-08-01 15:48:29 +0530

    Added previews

--- onboarding_screens/lib/main.dart
+++ onboarding_screens/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:onboarding_screens/models/onboardingModel.dart';
 import 'package:onboarding_screens/onboardingScreen.dart';
 import 'package:provider/provider.dart';
@@ -8,6 +9,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]);
     return ChangeNotifierProvider(
       create: (context) => HomeModel(),
       child: MaterialApp(
