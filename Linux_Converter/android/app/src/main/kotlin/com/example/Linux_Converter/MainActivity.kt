ad4fafe8d3e605e6c630aeb42434d98a0bcf2265
AshutoshPatole <apatole479@gmail.com>
2020-10-03 19:30:56 +0530

    Adding Linux Desktop app

---
+++ Linux_Converter/android/app/src/main/kotlin/com/example/Linux_Converter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.Linux_Converter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Linux_Converter/android/app/src/main/kotlin/com/example/converter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.converter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
