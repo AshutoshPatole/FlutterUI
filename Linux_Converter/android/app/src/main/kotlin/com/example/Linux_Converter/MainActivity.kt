fb82f6d72d1286c457222e3230837162ffe4a9e1
AshutoshPatole <apatole479@gmail.com>
2020-11-09 20:18:04 +0530

    removed Linux_Converter app:Incomplete

--- Linux_Converter/android/app/src/main/kotlin/com/example/Linux_Converter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.Linux_Converter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Linux_Converter/android/app/src/main/kotlin/com/example/converter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.converter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
